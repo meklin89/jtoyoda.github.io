43415eef1847bb6cced9e05f8f5cef373a3c5666
meklin89 <meklin89@gmail.com>
2025-08-16 20:44:32 -0500

    Add files via upload

--- scripts/autotracking.lua
+++ scripts/autotracking.lua
@@ -1,5 +1,5 @@
 -- Configuration --------------------------------------
-AUTOTRACKER_ENABLE_DEBUG_LOGGING = false
+AUTOTRACKER_ENABLE_DEBUG_LOGGING = true
 -------------------------------------------------------
 
 print("")
@@ -45,7 +45,7 @@ function updateToggleItemFromByteAndFlag(segment, code, address, flag)
     if item then
         local value = ReadU8(segment, address)
         if AUTOTRACKER_ENABLE_DEBUG_LOGGING then
-            print(item.Name, code, flag, value)
+            print(item.Name, code, flag, value, address, segment)
         end
 
         local flagTest = value & flag
